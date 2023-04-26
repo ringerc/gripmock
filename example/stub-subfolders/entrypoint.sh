169b205349327a98c6ba2dd9f82defcc5e34eadc
Craig Ringer <craig.ringer@enterprisedb.com>
2023-04-26 15:17:16 +1200

    Delete the handwritten clients
    These will be replaced with tests based on k6

--- example/stub-subfolders/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# this file is used by .github/workflows/integration-test.yml
-
-gripmock --stub=example/stub-subfolders/stub example/stub-subfolders/stub-subfolders.proto &
-
-go run example/stub-subfolders/client/*.go
